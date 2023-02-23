e726fddb475acfe93845f5e5861ea32eb77a72b4
Melanholik <Forliho@gmail.com>
2023-02-23 17:15:49 +0300

    js course task 1,2

---
+++ src/main/js/chapter2/homework/task2.js
@@ -0,0 +1,11 @@
+const storeName = "Fish store";
+const storeDescription = {
+    budget: 10000,
+    employees: ["Vitaliy", "Vasiliy", "Nicolay"],
+    products: {
+        vodka: 40,
+        beer: 10
+    },
+    open: true
+};
+
---
+++ src/main/js/chapter2/lesson7/js/script.js
@@ -0,0 +1,4 @@
+"use strict"
+console.log(1);
+const a = 15;
+console.log(a);
