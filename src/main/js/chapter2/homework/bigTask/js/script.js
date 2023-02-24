1eab040063250e487ce1628e40a29e2e7e09362d
Melanholik <Forliho@gmail.com>
2023-02-24 17:09:08 +0300

    Create new object personalMovieDB

--- src/main/js/chapter2/homework/bigTask/js/script.js
+++ src/main/js/chapter2/homework/bigTask/js/script.js
@@ -1,2 +1,10 @@
 "use strict"
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
